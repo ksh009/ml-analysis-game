4aa3b571fe472bce987964262fd228665e41724e
ksh009 <khughessean01@gmail.com>
2024-06-13 09:40:42 +0200

    confirm input and output details

--- score.js
+++ score.js
@@ -1,5 +1,10 @@
+const output = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  output.push([dropPosition, bounciness, size, bucketLabel])
+
+  console.log(output);
 }
 
 function runAnalysis() {
